7580dbf3f3565331bf21e9e02ed7eb8fd1074c43
Amber W <amber.wolf@outlook.com>
2021-01-01 14:09:06 -0700

    retrying the addition of http request

--- src/main/kotlin/com/am/petmanager/controller/PetAppController.kt
+++ src/main/kotlin/com/am/petmanager/controller/PetAppController.kt
@@ -1,4 +1,12 @@
 package com.am.petmanager.controller
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 class PetAppController {
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String):ResponseEntity<String>{
+        return ResponseEntity("Woof", HttpStatus.OK)
+    }
 }
